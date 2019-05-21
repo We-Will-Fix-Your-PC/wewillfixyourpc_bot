b5b3ab7889c917718d67c04eb80f6f244bd28685
Q Misell <q@misell.cymru>
2019-05-21 15:22:19 +0100

    broadcast new messages to operator ui

--- operator_interface/tasks.py
+++ operator_interface/tasks.py
@@ -1,13 +1,39 @@
 from celery import shared_task
 import dialogflow_client.tasks
 import facebook.tasks
+import twitter.tasks
+import pika
+import json
 from . import models
+
+_channel = None
+
+
+def get_channel():
+    global _channel
+    if _channel is not None:
+        return _channel
+    rmq = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = rmq.channel()
+    channel.exchange_declare(exchange='bot_messages', exchange_type='fanout')
+    _channel = channel
+    return channel
+
+
+@shared_task
+def send_message_to_interface(mid):
+    get_channel().basic_publish(exchange='bot_messages', routing_key='', body=json.dumps({
+        "mid": mid
+    }))
 
 
 @shared_task
 def process_message(mid):
     message = models.Message.objects.get(id=mid)
     conversation = message.conversation
+
+    send_message_to_interface.delay(mid)
 
     if message.direction == models.Message.FROM_CUSTOMER:
         if conversation.agent_responding:
