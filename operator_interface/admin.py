26187c610971bd6459bda57b359dbbd86f9478f9
Q Misell <q@misell.cymru>
2019-05-22 09:36:34 +0100

    store profile picture with user

--- operator_interface/admin.py
+++ operator_interface/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from . import models
 
 
+class UserProfileInline(admin.StackedInline):
+    model = models.UserProfile
+    can_delete = False
+    verbose_name_plural = 'User profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(models.Conversation)
 admin.site.register(models.ConversationRating)
 admin.site.register(models.Message)
--- operator_interface/models.py
+++ operator_interface/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 import secrets
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField()
+    fb_persona_id = models.CharField(max_length=255, blank=True, null=True)
 
 
 class Conversation(models.Model):
