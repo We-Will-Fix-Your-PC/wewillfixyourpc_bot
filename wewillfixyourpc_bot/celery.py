f7baf65286a4a144e6300482badb7a881a18510a
Q Misell <q@misell.cymru>
2019-07-31 12:42:00 +0100

    fix sentry

--- wewillfixyourpc_bot/celery.py
+++ wewillfixyourpc_bot/celery.py
@@ -3,10 +3,16 @@ import os
 from celery import Celery
 import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
-
-sentry_sdk.init("https://efc22f89d34a46d0adffb302181ed3f9@sentry.io/1471674", integrations=[CeleryIntegration()])
+from sentry_sdk.integrations.django import DjangoIntegration
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wewillfixyourpc_bot.settings')
+
+from django.conf import settings
+
+if not settings.DEBUG:
+    sentry_sdk.init("https://efc22f89d34a46d0adffb302181ed3f9@sentry.io/1471674",
+                    integrations=[CeleryIntegration(), DjangoIntegration()])
+
 app = Celery('wewillfixyourpc_bot')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
