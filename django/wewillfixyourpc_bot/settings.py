31076470733c7cebec67347888b3a39f416da9e1
Q Misell <q@misell.cymru>
2019-08-05 15:26:10 +0100

    host header from env

--- django/wewillfixyourpc_bot/settings.py
+++ django/wewillfixyourpc_bot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["bot.cardifftec.uk"]
+ALLOWED_HOSTS = [os.getenv("HOST", "bot.cardifftec.uk")]
 
 # Application definition
 INSTALLED_APPS = [
