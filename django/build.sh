1b8c7e196aaacaf90009aedff9103f5f6e51b0f3
Q 🦄 <q@misell.cymru>
2019-09-23 12:34:05 +0100

    improve build script

--- django/build.sh
+++ django/build.sh
@@ -3,18 +3,18 @@
 PAYMENT_PROVIDER="WORLDPAY";
 export PAYMENT_PROVIDER
 
-VERSION=$(sentry-cli releases propose-version)
+VERSION=$(sentry-cli releases propose-version || exit)
 
 cd react/operator_interface || exit
-yarn build
+yarn build || exit
 cd ../..
 
 cd react/payments_form || exit
-yarn webpack --config webpack.prod.js
+yarn webpack --config webpack.prod.js || exit
 cd ../..
 
-docker build -t "theenbyperor/wewillfixyourpcbot_django:$VERSION" .
-docker push "theenbyperor/wewillfixyourpcbot_django:$VERSION"
+docker build -t "theenbyperor/wewillfixyourpcbot_django:$VERSION" . || exit
+docker push "theenbyperor/wewillfixyourpcbot_django:$VERSION" || exit
 
-sentry-cli releases --org we-will-fix-your-pc new -p bot-server $VERSION
+sentry-cli releases --org we-will-fix-your-pc new -p bot-server $VERSION || exit
 sentry-cli releases --org we-will-fix-your-pc set-commits --auto $VERSION
--- django/start.sh
+++ django/start.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-python3.7 manage.py migrate
-python3.7 manage.py collectstatic --noinput
-
 exec "$@"
