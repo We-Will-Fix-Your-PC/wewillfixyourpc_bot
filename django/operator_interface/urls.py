52f18ef29adb3ad7e1bccdc9ebe19b4f77807a3f
Q 🦄 <q@misell.cymru>
2019-12-16 20:39:24 +0000

    persona picture url fix

--- django/operator_interface/urls.py
+++ django/operator_interface/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index),
     path("privacy/", views.privacy_policy),
     path("push_subscription/", views.push_subscription),
-    path("profile_pic/<int:user_id>/", views.profile_picture, name="profile_pic"),
+    path("profile_pic/<int:user_id>.jpg", views.profile_picture, name="profile_pic"),
     path("data/networks/", views.get_networks),
     path("data/brands/", views.get_brands),
     path("data/models/<brand>/", views.get_models),
